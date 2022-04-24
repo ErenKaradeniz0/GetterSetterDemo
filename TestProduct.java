0c5dbf6d1e19074a2d86535fedf541170f968f96
wiserenn <83872752+wiserenn@users.noreply.github.com>
2022-04-24 12:22:57 +0300

    Add files via upload

---
+++ TestProduct.java
@@ -0,0 +1,33 @@
+package SecondTermWeek0;
+
+public class TestProduct {
+
+    public static void main(String[] args) {
+
+       /*  instance variable
+         class variable
+        public private protected
+        getter setter
+        encapsulation
+        constructor*/
+
+        Product product = new Product();
+        product.setDescription("default");
+
+
+
+        product.setPrice(1000.0);
+        System.out.println(product.getDescription());
+        System.out.println(product.getId());
+        System.out.println(product.getPrice());
+
+        Product product2 = new Product(10, "pc", "asus", 1000, 10, "black");
+        System.out.println(product2.getId());
+        System.out.println(product2.getName());
+        System.out.println(product2.getDescription());
+        System.out.println(product2.getPrice());
+        System.out.println(product2.getStockAmount());
+        System.out.println(product2.getColour());
+        System.out.println(product2.getCode());
+    }
+}
